68822ee9ed7f6af1bf670fdd2c3acbae44a21349
Olai <olainavabai@gmail.com>
2026-03-08 17:22:45 +0100

    Fixed hotspots

--- users/src/app.ts
+++ users/src/app.ts
@@ -3,7 +3,17 @@ import cors from "cors";
 import { initDB } from "./database/database.js";
 
 const app = express();
-app.use(cors());
+
+// Disable X-Powered-By header to avoid disclosing framework version
+app.disable("x-powered-by");
+
+// Restrict CORS to known origins
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
 
 await initDB();
