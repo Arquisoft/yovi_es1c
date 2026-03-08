0203f7acf08ed7c43a6811f1499bab9aaa133498
raulvelasco <uo294946@uniovi.es>
2026-03-08 19:22:49 +0100

    Changed heuristic trait to add a new function called "name", which returns the heuristic's name

--- gamey/src/bot/heurisitic.rs
+++ gamey/src/bot/heurisitic.rs
@@ -3,4 +3,7 @@ use crate::{GameY, PlayerId};
 pub trait Heuristic {
     /// Evalúa un tablero desde la perspectiva del jugador `player`.
     fn evaluate(&self, board: &GameY, player: PlayerId) -> i32;
+
+    /// Devuelve el nombre del heurístico
+    fn name(&self) -> &str;
 }
