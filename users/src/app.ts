2be2b66a0d20883bea9b79f33710bbcead79b917
Olai <olainavabai@gmail.com>
2026-03-09 12:50:37 +0100

    Updated app.ts and index.ts

--- users/src/app.ts
+++ users/src/app.ts
@@ -16,10 +16,5 @@ app.use(cors({
 
 app.use(express.json());
 
-await initDB();
-
 export default app;
 
-app.listen(3000, () => {
-  console.log("Users running on port 3000");
-});
--- users/src/index.ts
+++ users/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import expressApp from "./app.js";
+import { initDB } from "./database/database.js";
 
 const app = express();
 app.disable('x-powered-by');
@@ -10,6 +12,9 @@ app.get('/', (req, res) => {
   res.send('Users Service (TypeScript) is running!');
 });
 
-app.listen(port, () => {
+await initDB();
+
+
+expressApp.listen(port, () => {
   console.log(`Users Service listening on port ${port}`);
 });
