9b0311b7e473a36d2427a2ea692a340667d17d80
raulvelasco <uo294946@uniovi.es>
2026-04-12 17:56:00 +0200

    i18n configuration config.ts file

---
+++ webapp/src/i18n/config.ts
@@ -0,0 +1,5 @@
+export const SUPPORTED_LANGUAGES = ['en', 'es'] as const;
+export const DEFAULT_LANGUAGE = 'en';
+
+export const NAMESPACES = ['common'] as const;
+export const DEFAULT_NAMESPACE = 'common';
