aca00a79b98b30ae375bb84f30d7f730b89ba9f4
raulvelasco <uo294946@uniovi.es>
2026-04-24 19:02:05 +0200

    Updated users database to include display_name and email

--- users/src/database/users.sql
+++ users/src/database/users.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS user_profiles (
     user_id INTEGER PRIMARY KEY ,
     username TEXT UNIQUE NOT NULL,
+    display_name TEXT,
+    email TEXT,
     avatar TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 );
